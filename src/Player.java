1725a76be25255a6f1ba03c62e2cd2592c99e1b2
freshmen <freshmen@damien-hs.edu>
2022-11-09 12:50:41 -0800

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,50 @@
+//
+// This class acts like a blueprint.
+// It itself is not a player, but describes and allows the creation of player
+//
+
+public class Player {
+
+    // fields, instance variables, attributes (has but can be different, name, etc)
+    private String name;
+    private int health;
+    private int dexterity; // how well a player uses dex weapons (bows, daggers)
+    private int strength; // how well a player uses str weapons (swords, axes)
+    private int intelligence; // how well a player can cast a spell
+
+    // constructors, how to create new Players
+
+
+    // methods, actions
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void setHeath(int health) {
+        this.health = health;
+    }
+    public int getDexterity() {
+        return dexterity;
+    }
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getIntelligence() {
+        return intelligence;
+    }
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+}
