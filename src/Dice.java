a25cd9beec5c7180818d6b27d0584de5b93a8295
William <wbmorris24@damien-hs.edu>
2022-11-14 12:09:55 -0800

    More Class stuff

--- src/Dice.java
+++ src/Dice.java
@@ -1,7 +1,5 @@
 public class Dice {
     public static int roll(String stuff) {
-
-
         int placeOfD = stuff.indexOf("d");
         int numOfDice = Integer.parseInt(stuff.substring(0, placeOfD));
         int sides = Integer.parseInt(stuff.substring(placeOfD + 1));
@@ -11,6 +9,4 @@ public class Dice {
         }
         return total;
     }
-
-
 }
